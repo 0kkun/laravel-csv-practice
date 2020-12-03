6617d861a8dc177473423f6e71f353352d2e7025
0kkun <shinji.okumoto.01@gmail.com>
2020-12-04 08:24:59 +0900

    create view for csv

---
+++ server/resources/views/csv/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('content')
+    <div class="container-fluid">
+        CSV Practice Page
+    </div>
+@endsection
---
+++ server/resources/views/layouts/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ja">
+    <head>
+        <title>CSV Practice</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js')}}"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    </head>
+    <body>
+        <main>
+            @yield('content')
+        </main>
+    </body>
+</html>
