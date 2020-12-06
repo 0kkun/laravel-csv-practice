aa510221a7ba55fa0750a854e45645f2b6ecf66c
0kkun <shinji.okumoto.01@gmail.com>
2020-12-06 20:57:33 +0900

    edit index file

--- server/resources/views/csv/index.blade.php
+++ server/resources/views/csv/index.blade.php
@@ -1,8 +1,15 @@
 @extends('layouts.layout')
 
 @section('content')
+    @if (session('error')) 
+        <div class="alert alert-danger">{{ session('error') }}</div>
+    @endif
+
     <div class="container-fluid">
         CSV Practice Page
     </div>
-    <div class="btn btn-success p-1 m-2"><a href="{{ route('csv.export') }}">CSV DOWNLOAD</a></div>
+    <form action="{{ route('csv.download') }}" method="post">
+        {{ csrf_field()}}
+        <button type="submit" class="btn btn-success p-1 m-2">CSV DOWNLOAD</button>
+    </form>
 @endsection
